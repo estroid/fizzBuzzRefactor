f2468c8368533a47fc6e92992e30d7300553840c
estroid <estroid@gmail.com>
2016-02-02 12:44:07 -0500

    begin function

---
+++ custom.js
@@ -0,0 +1,11 @@
+
+// var integer = prompt('Please input a number.'); // user is prompted to supply a number.
+
+function integerArgument (integer) {
+
+    for (var x = 1; x <= integer; x++) {
+
+// put fizzbuzz for loop
+
+    }
+}
